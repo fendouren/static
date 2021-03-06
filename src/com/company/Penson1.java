7a34f5533ca376df2203ea5c245154702322b6d5
qinyadong <1147918652@qq.com>
2021-03-06 20:30:52 +0800

    1111

---
+++ src/com/company/Penson1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * @author qinyadong
+ * @create 2021-03-06 20:19
+ */
+public class Penson1 {
+
+    public static void main(String[] args) {
+        System.out.println("我走了");
+        System.out.println("我走了");
+    }
+
+}
